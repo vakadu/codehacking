070f86b57e7e259492b83087dc67c181efabc783
Zoro <vakadu10@gmail.com>
2016-11-06 14:34:22 +0530

    lecture 202

--- resources/views/admin/users/create.blade.php
+++ resources/views/admin/users/create.blade.php
@@ -3,7 +3,7 @@
 @section('content')
     <h1>Create User</h1>
 
-    {!! Form::open(['method'=>'POST','action'=>'AdminUsersController@store']) !!}
+    {!! Form::open(['method'=>'POST','action'=>'AdminUsersController@store', 'files' => true]) !!}
     <div class="form-group">
         {!! Form::label('name', 'Name:') !!}
         {!! Form::text('name', null, ['class'=>'form-control'])!!}
@@ -20,14 +20,14 @@
     </div>
 
     <div class="form-group">
-        {!! Form::label('status', 'Status:') !!}
-        {!! Form::select('status', array(1 => 'Active', 0 =>'Not Active'), 0, ['class'=>'form-control'])!!}
+        {!! Form::label('is_active', 'Status:') !!}
+        {!! Form::select('is_active', array(1 => 'Active', 0 =>'Not Active'), 0, ['class'=>'form-control'])!!}
     </div>
 
-    {{--<div class="form-group">--}}
-        {{--{!! Form::label('photo_id', 'Image:') !!}--}}
-        {{--{!! Form::file('photo_id', null, ['class'=>'form-control'])!!}--}}
-    {{--</div>--}}
+    <div class="form-group">
+        {!! Form::label('file', 'Title:') !!}
+        {!! Form::file('file', null, ['class'=>'form-control'])!!}
+    </div>
 
     <div class="form-group">
         {!! Form::label('password', 'Password:') !!}
